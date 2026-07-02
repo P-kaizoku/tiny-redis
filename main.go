a2ea72fb451afc4816f7f7096a9f4196731831f0
pabitra <pabitram322@gmail.com>
2026-07-02 17:11:35 +0530

    feat: build server

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello tiny-redis")
+
+	listener, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer conn.Close()
+
+	for {
+		buff := make([]byte, 1024)
+
+		_, err := conn.Read(buff)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("Error in reading from the client", err.Error())
+			os.Exit(1)
+		}
+
+		conn.Write([]byte("+OK\r\n"))
+	}
 }
