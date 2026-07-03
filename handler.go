4d96f7346036130ebf1cc6c1e18f8feb1d3edc8e
pabitra <pabitram322@gmail.com>
2026-07-03 13:04:26 +0000

    add handler func for SET command

--- handler.go
+++ handler.go
@@ -1,8 +1,18 @@
 package main
 
-var Handlers = map[string]func([]Value) Value{
-	"PING": ping,
-}
+import (
+	"sync"
+)
+
+var (
+	Handlers = map[string]func([]Value) Value{
+		"PING": ping,
+		"SET":  set,
+	}
+
+	SETs   = map[string]string{}
+	SETsMu = sync.RWMutex{}
+)
 
 func ping(args []Value) Value {
 	if len(args) == 0 {
@@ -10,4 +20,19 @@ func ping(args []Value) Value {
 		return Value{typ: "string", str: "PONG"}
 	}
 	return Value{typ: "string", str: args[0].bulk}
+}
+
+func set(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "error: wrong no. of arguments for SET command. Requires 2 only."}
+	}
+
+	key := args[0].bulk
+	val := args[0].bulk
+
+	SETsMu.Lock()
+	SETs[key] = val
+	SETsMu.Unlock()
+
+	return Value{typ: "string", str: "OK"}
 }
